59358a2fa8810a7a40deabd06c5e329aa81164a0
Michael-Reeve <m.d.reeve.98@gmail.com>
2019-02-12 00:00:01 +0000

    Persistent Levels

--- Dissertation/Assets/Resources/Programming/Framework/GameManager.cs
+++ Dissertation/Assets/Resources/Programming/Framework/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
 	public bool playingGame;
 	public bool loadingLevel;
     private int level = 3;
+	public Dictionary<string, ObjectData> levelDictionary = new Dictionary<string, ObjectData>();
 
 	void Awake()
 	{
@@ -22,12 +23,29 @@ public class GameManager : MonoBehaviour
 	public AsyncOperation LoadLevel(string levelName)
 	{
 		SaveGame.Save();
+		JSON.Save(SceneManager.GetActiveScene().name + ".txt", new LevelContainer(levelDictionary));
+		Debug.Log("LEVEL: " + JsonUtility.ToJson(new LevelContainer(levelDictionary)));
 		AsyncOperation async = SceneManager.LoadSceneAsync(levelName);
 		loadingLevel = true;
 		StartCoroutine(WaitForLoad(async, levelName));
 		return async;
 	}
 
+	public void AddLevelData(string keyObject, ObjectData objectData)
+	{
+		levelDictionary.Add(keyObject, objectData);
+	}
+
+	public void LoadLevelData(LevelContainer copyData)
+	{
+		Debug.Log(copyData.keys.Count);
+		levelDictionary.Clear();
+		for(int i = 0; i < copyData.keys.Count; i++)
+		{
+			Debug.Log(copyData.values[i].position);
+			levelDictionary.Add(copyData.keys[i], copyData.values[i]);
+		}
+	}
 
 	protected IEnumerator WaitForLoad(AsyncOperation async, string levelName)
     {
@@ -44,10 +62,30 @@ public class GameManager : MonoBehaviour
 
 	protected void SceneLoaded(string levelName)
 	{
+		Debug.Log(levelDictionary.Count);
+		if(JSON.CheckSave(SceneManager.GetActiveScene().name +".txt"))
+			LoadLevelData(JSON.Load<LevelContainer>(SceneManager.GetActiveScene().name + ".txt"));
+		Debug.Log(levelDictionary.Count);
 		if(levelName != "MainMenu")
 			playingGame = true;
 		else
 			playingGame = false;
 	}
 	
+}
+public class LevelContainer
+{
+	public List<string> keys = new List<string>();
+	public List<ObjectData> values = new List<ObjectData>();
+	public LevelContainer(Dictionary<string, ObjectData> _levelDictionary)
+	{
+		foreach(string key in _levelDictionary.Keys)
+		{
+			keys.Add(key);
+		}
+		foreach(ObjectData value in _levelDictionary.Values)
+		{
+			values.Add(value);
+		}
+	}
 }
