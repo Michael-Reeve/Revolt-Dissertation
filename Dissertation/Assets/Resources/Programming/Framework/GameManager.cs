5c5befa6d748f4442a74fdd51d50cdd9680dfc6a
Kyle Scott <Mcr_to@users.noreply.github.com>
2019-02-13 10:44:56 +0000

    Whitebox level update3.0
    Added hole in floor and lowered the amount of floor space as a whole, changed some rooms to allow for a better flow

---
+++ Dissertation/Assets/Resources/Programming/Framework/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour 
+{
+	public static GameManager instance = null;
+    private int level = 3;
+
+	void Awake()
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject);    
+		
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public AsyncOperation LoadLevel(string levelName)
+	{
+		SaveGame.Save();
+		AsyncOperation async = SceneManager.LoadSceneAsync(levelName);
+		StartCoroutine(WaitForLoad(async));
+		return async;
+	}
+
+	protected static IEnumerator WaitForLoad(AsyncOperation async)
+    {
+		while (!async.isDone) 
+		{
+			//Debug.Log(async.progress);
+            yield return null;
+        }
+		SaveGame.Load();
+    }
+}
---
+++ Dissertation/Assets/Resources/Programming/UI/SaveGame.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SaveGame 
+{
+	public static void Save()
+	{
+		List<GameObject> rootObjects = new List<GameObject>();
+		Scene scene = SceneManager.GetActiveScene();
+		scene.GetRootGameObjects(rootObjects);
+		List<ISave> saves = new List<ISave>();
+		foreach (GameObject gameObject in rootObjects)
+		{
+			saves.AddRange(Utility.GetInterface<ISave>(gameObject.GetComponentsInChildren(typeof(ISave), true)));
+		}
+		for(int i = 0; i < saves.Count; i++)
+		{
+			saves[i].Save();
+		}
+	}
+
+	public static void Load()
+	{
+		List<GameObject> rootObjects = new List<GameObject>();
+		Scene scene = SceneManager.GetActiveScene();
+		scene.GetRootGameObjects(rootObjects);
+		List<ISave> saves = new List<ISave>();
+		foreach (GameObject gameObject in rootObjects)
+		{
+			saves.AddRange(Utility.GetInterface<ISave>(gameObject.GetComponentsInChildren(typeof(ISave), true)));
+		}
+		for(int i = 0; i < saves.Count; i++)
+		{
+			saves[i].Load();
+		}
+	}
+	
+	public static void SaveVector3(Vector3 position, string key)
+	{
+		PlayerPrefs.SetFloat(key + "x", position.x);
+		PlayerPrefs.SetFloat(key + "y", position.y);
+		PlayerPrefs.SetFloat(key + "z", position.z);
+	}
+
+	public static Vector3 LoadVector3(string key)
+	{
+		return new Vector3(PlayerPrefs.GetFloat(key + "x"), PlayerPrefs.GetFloat(key + "y"), PlayerPrefs.GetFloat(key + "z"));
+	}
+
+	public static void SaveQuaternion(Quaternion rotation, string key)
+	{
+		PlayerPrefs.SetFloat(key + "x", rotation.x);
+		PlayerPrefs.SetFloat(key + "y", rotation.y);
+		PlayerPrefs.SetFloat(key + "z", rotation.z);
+		PlayerPrefs.SetFloat(key + "w", rotation.w);
+	}
+
+	public static Quaternion LoadQuaternion(string key)
+	{
+		return new Quaternion(PlayerPrefs.GetFloat(key + "x"), PlayerPrefs.GetFloat(key + "y"), PlayerPrefs.GetFloat(key + "z"), PlayerPrefs.GetFloat(key + "w"));
+	}
+}
