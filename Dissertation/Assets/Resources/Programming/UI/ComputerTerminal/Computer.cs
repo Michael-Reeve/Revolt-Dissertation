0a49637316de9036dd5ba232ec59b9a0795450c7
Michael-Reeve <m.d.reeve.98@gmail.com>
2019-02-13 03:08:24 +0000

    Getting monitors to respond to input

--- Dissertation/Assets/Resources/Programming/UI/ComputerTerminal/Computer.cs
+++ Dissertation/Assets/Resources/Programming/UI/ComputerTerminal/Computer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Computer : MonoBehaviour, Interactible
@@ -8,11 +9,27 @@ public class Computer : MonoBehaviour, Interactible
 	public bool lockedOn;
 	public Vector2 cursorPosition;
 	public RectTransform cursor;
+	public GraphicRaycaster graphicRaycaster;
+	public EventSystem eventSystem;
+
+	void Awake()
+	{
+		//graphicRaycaster = GetComponent<GraphicRaycaster>();
+	}
 
 	public void Interact(PlayerController controller)
 	{
-		RaycastHit raycastHit = controller.Raycast();
-		Debug.Log(raycastHit.collider);
+		PointerEventData pointerEventData = new PointerEventData(eventSystem);
+		pointerEventData.position = Input.mousePosition;
+		List<RaycastResult> results = new List<RaycastResult>();
+		graphicRaycaster.Raycast(pointerEventData, results);
+		foreach(RaycastResult result in results)
+		{
+			if(result.gameObject.GetComponent<Button>())
+			{
+				result.gameObject.GetComponent<Button>().OnPointerClick(pointerEventData);
+			}
+		}
 	}
 
 	public void SetCursorPosition()
