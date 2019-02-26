dbac9976adae150f0425770fc1adda5815ac8b1a
Michael-Reeve <m.d.reeve.98@gmail.com>
2019-02-26 04:58:10 +0000

    tiny tweak to headbob script

--- Dissertation/Assets/Resources/Programming/Framework/Camera/Headbob.cs
+++ Dissertation/Assets/Resources/Programming/Framework/Camera/Headbob.cs
@@ -10,7 +10,7 @@ public class Headbob : MonoBehaviour
 	void LateUpdate()
 	{
 		velocity = cameraController.possessed.movementVelocity * 100;
-		notsure = Mathf.Sin(Time.time * Mathf.Round(velocity.magnitude) * 1.5f) / 25;
+		notsure = Mathf.Sin(Time.time * Mathf.Round(velocity.magnitude) * 1.25f) / 25;
 		transform.position = new Vector3(transform.position.x, transform.position.y + notsure, transform.position.z);
     	transform.eulerAngles = new Vector3(transform.eulerAngles.x + (notsure * 5), transform.eulerAngles.y, transform.eulerAngles.z);
 	}
