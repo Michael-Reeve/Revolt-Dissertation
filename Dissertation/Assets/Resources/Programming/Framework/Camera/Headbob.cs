dea9edf69c1029e29f49fd297099cd696cc8cbb2
Michael-Reeve <m.d.reeve.98@gmail.com>
2019-03-21 23:46:40 +0000

    Footsteps, Merged Kyles + newest level,

--- Dissertation/Assets/Resources/Programming/Framework/Camera/Headbob.cs
+++ Dissertation/Assets/Resources/Programming/Framework/Camera/Headbob.cs
@@ -6,12 +6,12 @@ public class Headbob : MonoBehaviour
 {
 	public FirstPersonCameraController cameraController;
 	public Vector3 velocity;
-	public float notsure;
+	public float cameraOffset;
 	void LateUpdate()
 	{
 		velocity = cameraController.possessed.movementVelocity * 100;
-		notsure = Mathf.Sin(Time.time * Mathf.Round(velocity.magnitude) * 1.25f) / 25;
-		transform.position = new Vector3(transform.position.x, transform.position.y + notsure, transform.position.z);
-    	transform.eulerAngles = new Vector3(transform.eulerAngles.x + (notsure * 5), transform.eulerAngles.y, transform.eulerAngles.z);
+		cameraOffset = Mathf.Sin(Time.time * Mathf.Round(velocity.magnitude) * 1.25f) / 25;
+		transform.position = new Vector3(transform.position.x, transform.position.y + cameraOffset, transform.position.z);
+    	transform.eulerAngles = new Vector3(transform.eulerAngles.x + (cameraOffset * 5), transform.eulerAngles.y, transform.eulerAngles.z);
 	}
 }
---
+++ Dissertation/Assets/Resources/Programming/Scripts/FootstepNoise.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepNoise : MonoBehaviour 
+{
+
+	public AudioSource audioSource;
+	public Headbob headbob;
+	[Range (1,-1)]
+	public int offsetTrigger;
+	public List<AudioClip> footsteps;
+	private bool stepAvailable;
+	
+	void Update () 
+	{
+		if(Mathf.Round(Mathf.Sin(headbob.velocity.magnitude * Time.time / 3)) == offsetTrigger)
+		{
+			stepAvailable = false;
+			audioSource.clip = footsteps[(int)Random.Range(0, footsteps.Count)];
+			audioSource.Play();
+		}
+		if(Mathf.Round(Mathf.Sin(headbob.velocity.magnitude * Time.time / 3)) == (offsetTrigger * -1))
+		{
+			stepAvailable = true;
+		}
+	}
+}
