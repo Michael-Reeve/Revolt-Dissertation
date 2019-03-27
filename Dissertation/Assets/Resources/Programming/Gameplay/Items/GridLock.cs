74262296de5fe71a0abf7b83676e0531c1b21935
Michael-Reeve <m.d.reeve.98@gmail.com>
2019-03-27 01:01:01 +0000

    Altered Grid Lock to be more responsive (will optimize later), changed a couple of sounds and edited the level a little.

--- Dissertation/Assets/Resources/Programming/Gameplay/Items/GridLock.cs
+++ Dissertation/Assets/Resources/Programming/Gameplay/Items/GridLock.cs
@@ -9,20 +9,19 @@ public class GridLock : MonoBehaviour
 	public int highlight;
 	public Vector3 offset;
 	private Vector3 currentPosition;
-	public bool overlapping;
+	public bool overlapping = true;
+	public LayerMask ignoreLayers;
 	private Color originalColor;
+	private MeshRenderer meshRend;
 	void Awake()
 	{
-		MeshRenderer meshRend = GetComponentInChildren<MeshRenderer>();
+		meshRend = GetComponentInChildren<MeshRenderer>();
 		originalColor = meshRend.material.color;
-		meshRend.material.color = Color.blue;
-		meshRend.material.SetColor("_EmissionColor", originalColor + new Color(0, 0, 1, 0));
+		SetOverlapColor(overlapping);
 	}
 
 	void Update () 
 	{
-		transform.position = origin.transform.position + (origin.transform.forward * offset.z);
-		transform.position = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y), Mathf.Round(transform.position.z));
 		if(controller != null)
 		{
 			if(controller.inventory.GUI.highlightedItem != highlight)
@@ -30,26 +29,37 @@ public class GridLock : MonoBehaviour
 				Destroy(this.gameObject);
 			}
 		}
-	}
 
-    void OnTriggerEnter(Collider other)
-    {
-		if(other.gameObject.layer == this.gameObject.layer)
+		transform.position = origin.transform.position + (origin.transform.forward * offset.z);
+		transform.position = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y), Mathf.Round(transform.position.z));
+
+		Collider[] collidersArray = new Collider[2];
+		Physics.OverlapSphereNonAlloc(transform.position, 0.1f, collidersArray, ignoreLayers, QueryTriggerInteraction.Collide);
+		if(collidersArray[0] != null && collidersArray[1] == null)
 		{
-			MeshRenderer meshRend = GetComponentInChildren<MeshRenderer>();
-			meshRend.material.color = Color.red;
-			meshRend.material.SetColor("_EmissionColor", originalColor + new Color(1, 0, 0, 0));
-			overlapping = true;
-		}
-    }
-	void OnTriggerExit(Collider other)
-    {
-		if(other.gameObject.layer == this.gameObject.layer)
-		{
-			MeshRenderer meshRend = GetComponentInChildren<MeshRenderer>();
-			meshRend.material.color = Color.blue;
-			meshRend.material.SetColor("_EmissionColor", originalColor + new Color(0, 0, 1, 0));
+			Debug.Log(collidersArray[0]);
 			overlapping = false;
 		}
-    }
+		else
+		{
+			Debug.Log(collidersArray[0] + " | " + collidersArray[1]);
+			overlapping = true;
+		}
+		SetOverlapColor(overlapping);
+	}
+
+	public void SetOverlapColor(bool cantBePlaced)
+	{
+		if(cantBePlaced && meshRend.material.color != Color.red)
+		{
+			meshRend.material.color = Color.red;
+			meshRend.material.SetColor("_EmissionColor", originalColor + new Color(1, 0, 0, 0));
+		}
+		else if (!cantBePlaced && meshRend.material.color != Color.blue)
+		{
+			meshRend.material.color = Color.blue;
+			meshRend.material.SetColor("_EmissionColor", originalColor + new Color(0, 0, 1, 0));
+		}
+	}
+
 }
